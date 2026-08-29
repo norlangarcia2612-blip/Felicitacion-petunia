e0059585f6d65c29ccd658c8d8c756f2a79d863c
norlangarcia2612-blip <norlangarcia2612@gmail.com>
2026-08-29 14:49:05 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const openBtn = document.getElementById("openBtn");
+    const sorpresa = document.getElementById("sorpresa");
 
+    if (openBtn && sorpresa) {
+        openBtn.addEventListener("click", function () {
+            sorpresa.classList.remove("hidden");
+
+            sorpresa.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+
+            openBtn.style.display = "none";
+        });
+    }
+});
